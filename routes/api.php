61d3cfe909edb8348e4100061aa8009f54b7a755
Junior <jmota.wk@hotmail.com>
2021-12-23 08:20:42 -0300

    add: me route.

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,11 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth.jwt');
+
+    Route::group(['middleware' => 'auth.jwt'], function () {
+        Route::get('me', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
 
 Route::group(['middleware' => 'auth.jwt'], function () {
