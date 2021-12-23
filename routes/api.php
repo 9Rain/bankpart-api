28ea22981601ead67e9f425d74450de8d527ce68
Junior <jmota.wk@hotmail.com>
2021-12-23 09:09:38 -0300

    update: added version prefix to api routes.

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\api\{
     PartitionController
 };
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,28 +20,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+
+        Route::group(['middleware' => 'auth.jwt'], function () {
+            Route::get('me', [AuthController::class, 'me']);
+            Route::post('logout', [AuthController::class, 'logout']);
+        });
+    });
 
     Route::group(['middleware' => 'auth.jwt'], function () {
-        Route::get('me', [AuthController::class, 'me']);
-        Route::post('logout', [AuthController::class, 'logout']);
-    });
-});
+        Route::apiResource('users', UserController::class);
 
-Route::group(['middleware' => 'auth.jwt'], function () {
-    Route::apiResource('users', UserController::class);
+        Route::prefix('users/{userId}')->group(function () {
+            Route::apiResource('accounts', AccountController::class);
 
-    Route::prefix('users/{userId}')->group(function () {
-        Route::apiResource('accounts', AccountController::class);
+            Route::prefix('accounts/{accountId}')->group(function () {
+                Route::apiResource('partitions', PartitionController::class);
 
-        Route::prefix('accounts/{accountId}')->group(function () {
-            Route::apiResource('partitions', PartitionController::class);
-
-            Route::prefix('partitions/{id}')->group(function () {
-                Route::patch('/addMoney', [PartitionController::class, 'addMoney']);
-                Route::patch('/removeMoney', [PartitionController::class, 'removeMoney']);
+                Route::prefix('partitions/{id}')->group(function () {
+                    Route::patch('/addMoney', [PartitionController::class, 'addMoney']);
+                    Route::patch('/removeMoney', [PartitionController::class, 'removeMoney']);
+                });
             });
         });
     });
